0d840c52d7008ab023d2dc56942af1c376a463d4
Dave <orangechannel@pm.me>
2020-07-29 16:08:07 -0400

    Update documentation

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = ''
 author = 'Dave <orangechannel@pm.me>'
 
 # The full version, including alpha/beta/rc tags
-version = release = '5.0.0'
+version = release = '5.0.1'
 
 
 # -- General configuration ---------------------------------------------------
