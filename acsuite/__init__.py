9244083aca27d67ade8771f9ed1372b6fe29ac2c
Dave <orangechannel@pm.me>
2020-09-01 01:59:39 -0400

    Fix mixing '.' in recently added w64 extension

--- acsuite/__init__.py
+++ acsuite/__init__.py
@@ -109,7 +109,7 @@ def eztrim(clip: vs.VideoNode,
             '.sbc',
             '.thd',
             '.tta',
-            '.wav', 'w64',
+            '.wav', '.w64',
             '.wma',
         }
         if audio_file_ext not in ffmpeg_valid_encoder_extensions:
