79b7001ef0f5c16d10eb9ced1160384eb1500f9a
Dave <orangechannel@pm.me>
2020-09-27 00:27:26 -0400

    Version and date bump

--- acsuite/_metadata.py
+++ acsuite/_metadata.py
@@ -1,10 +1,10 @@
 from datetime import date
 
 __author__ = 'Dave <orangechannel@pm.me>'
-__date__ = date.fromisoformat('2020-09-25')
+__date__ = date.fromisoformat('2020-09-27')
 __credits__ = """AzraelNewtype, for the original audiocutter.py.
 Ricardo Constantino (wiiaboo), for vfr.py from which this was inspired.
 doop, for explaining the use of None for empty slicing.
 Vardë, for fixing FFmpeg's >4GB WAV file issues.
 """
-__version__ = '5.4.1'
+__version__ = '6.0.0-alpha'
